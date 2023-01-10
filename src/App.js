f6de7388e75fec85266dcc2bd55c42896d2166ca
K M Saddam Hossain <dev.saddam@gmail.com>
2023-01-10 14:41:13 +0600

    Change State

--- src/App.js
+++ src/App.js
@@ -4,19 +4,53 @@ import "./App.css";
 import Book from "./components/Book";
 
 class App extends Component {
+  state = {
+    books: [
+      { bookName: "No Border", writter: "Alen Cross" },
+      { bookName: "Yellow", writter: "Bisnu Dey" },
+      { bookName: "Black War", writter: "Mrinmoye Gosh" },
+    ],
+    otherContent: "I am other Content",
+  };
+
+  changeBookState = () => {
+    // console.log("Clicked Me!");
+    this.setState({
+      books: [
+        { bookName: "Last Border", writter: "Cosmoy Bill" },
+        { bookName: "Sharlok Homes", writter: "Arobindu Gosh" },
+        { bookName: "Raj Prashad", writter: "Omol Kanti" },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h2>Book List</h2>
 
-        <Book bookName="Nil Somudro" writter="Alen Roy" />
-        <Book bookName="Yellow Journalism" writter="Bisnu Dey" />
-        <Book bookName="Black War" writter="Mrinmoye Gosh" />
+        <Book
+          bookName={this.state.books[0].bookName}
+          writter={this.state.books[0].writter}
+        />
+        <Book
+          bookName={this.state.books[1].bookName}
+          writter={this.state.books[1].writter}
+        />
+        <Book
+          bookName={this.state.books[2].bookName}
+          writter={this.state.books[2].writter}
+        />
+
+        <p>{this.state.otherContent}</p>
+
         {/* <Person name="A" age="25">
           Basic info
         </Person>
         <Person name="B" age="28" />
         <Person name="C" age="35" /> */}
+
+        <button onClick={this.changeBookState}>Change State</button>
       </div>
     );
   }
